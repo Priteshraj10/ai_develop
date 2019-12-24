a9eed6eb31a201b84fa31e09611b67405593d84e
priteshraj10 <priteshraj41@gmail.com>
2019-12-24 09:49:32 +0530

    Delete all the stuff

--- play.py
+++
@@ -1,9 +0,0 @@
-import torch
-from state import State
-
-
-if __name__ == '__main__':
-    model = torch.load('nets/value.pth', map_location=lambda storage, loc:storage)
-    s = State()
-    output = model(s.serialize())
-    print(output)
